433d085ad7ae71b1d91051c059877b034aee7606
UAhmadSoft <umadahmad1928@gmail.com>
2022-01-15 10:39:08 +0500

    Added TodoList Sync With localstorage

---
+++ src/hooks/useTodoState.js
@@ -0,0 +1,29 @@
+import { v4 } from 'uuid';
+import { useLocalStorageState } from './useLocalStorageState';
+export default (initialTodos) => {
+  const [todos, setTodos] = useLocalStorageState('todos', initialTodos);
+  return {
+    todos,
+    addTodo: (newTodoText) => {
+      setTodos([...todos, { id: v4(), task: newTodoText, completed: false }]);
+    },
+    removeTodo: (todoId) => {
+      //filter out removed todo
+      const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+      //call setTodos with new todos array
+      setTodos(updatedTodos);
+    },
+    toggleTodo: (todoId) => {
+      const updatedTodos = todos.map((todo) =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+      );
+      setTodos(updatedTodos);
+    },
+    editTodo: (todoId, newTask) => {
+      const updatedTodos = todos.map((todo) =>
+        todo.id === todoId ? { ...todo, task: newTask } : todo
+      );
+      setTodos(updatedTodos);
+    },
+  };
+};
